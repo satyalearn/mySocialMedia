6163f473f5e967fd027001dd430f7ed57000a43c
satyalearn <88238405+satyalearn@users.noreply.github.com>
2023-03-29 15:53:19 +0530

    google authentication is done with help of firebase

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import { auth } from '../config/firebase'
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {signOut} from 'firebase/auth'
 
 const Navbar = () => {
+  const [user] = useAuthState(auth);
+
+  const signUserOut =async()=>{
+    await signOut(auth);
+  }
   return (
-    <div>
+    <div className='navbar'>
+      <div className='links'>
         <Link to='/'> Home</Link>
         <Link to='/login'> Login</Link>
-
-        <div>
-          <p>{auth.currentUser?.displayName}</p>
-          <img src={auth.currentUser?.photoURL || ""} width='50px' height='50px' />
+        </div>
+        <div className='user'>
+          { user && (<><p>{user?.displayName}</p>
+          <img src={user?.photoURL || ""} width='50px' height='50px' />
+          <button className='logout' onClick={signUserOut}>Log Out</button></>)}
+          
         </div>
     </div>
   )
